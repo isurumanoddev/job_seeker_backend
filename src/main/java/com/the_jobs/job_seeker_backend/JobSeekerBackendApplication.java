e41beb5ff0ef43cc6b82e4f4633099adbe0aec35
isuruUpview <isuru.senanayake@upview.tech>
2023-09-04 19:31:22 +0530

    Create Entities / create DTO s

--- src/main/java/com/the_jobs/job_seeker_backend/JobSeekerBackendApplication.java
+++ src/main/java/com/the_jobs/job_seeker_backend/JobSeekerBackendApplication.java
@@ -1,13 +1,19 @@
 package com.the_jobs.job_seeker_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JobSeekerBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JobSeekerBackendApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
