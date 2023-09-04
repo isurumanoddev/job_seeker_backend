c804e999b06250f3a9aabb5af31cc9522c0a82fb
isuruUpview <isuru.senanayake@upview.tech>
2023-09-04 16:54:37 +0530

    Initial commit

---
+++ src/main/java/com/the_jobs/job_seeker_backend/JobSeekerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.the_jobs.job_seeker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSeekerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/the_jobs/job_seeker_backend/JobSeekerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_jobs.job_seeker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
